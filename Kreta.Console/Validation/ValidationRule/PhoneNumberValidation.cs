80312ccca8799cec77df5a80ca4e342252a1419b
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-10 09:44:16 +0200

    A feladat

---
+++ Kreta.Console/Validation/ValidationRule/PhoneNumberValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta.Console.Validation.ValidationRule
+{
+    public class PhoneNumberValidation
+    {
+        // Valid phone number: "ddd-ddd"
+        private readonly string _phoneNumber;
+        public PhoneNumberValidation(string phoneNumber)
+        {
+            _phoneNumber = phoneNumber;
+        }
+
+        public bool IsExactlySevenLong => false;
+        public bool IsDashInMiddle => false;
+        public bool IsThreNumberAtTheBeggining =>false;
+        public bool IsThreNumberAtTheEnd => false;
+    }
+}
---
+++ Kreta.Console/Validation/ValidationRule/PinCodeValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta.Console.Validation.ValidationRule
+{
+    public class PinCodeValidation
+    {
+        private readonly string _pinCode;
+        public PinCodeValidation(string pinCode)
+        {
+            _pinCode = pinCode;
+        }
+
+        public bool IsFourCharactersLengths => false;
+        public bool IsOnlyDecimalNumber => false;
+    }
+}
